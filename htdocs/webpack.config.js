91e11e1e78b0d16b51525ce4f9095575f4605b15
Alex Rans <alexrans@protonmail.com>
2023-04-12 19:38:17 +0200

    polaroids :3

---
+++ htdocs/webpack.config.js
@@ -0,0 +1,25 @@
+const Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/scripts/app.js')
+    .copyFiles({
+        from: './assets/img',
+        to: 'img/[path][name].[ext]'
+    })
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableSassLoader()
+    .autoProvidejQuery();
+
+module.exports = Encore.getWebpackConfig();

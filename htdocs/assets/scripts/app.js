7aa539463a47ab615f126768decb382e403cae60
Alex Rans <alexrans@protonmail.com>
2023-04-14 20:10:05 +0200

    Navigation

--- htdocs/assets/scripts/app.js
+++ htdocs/assets/scripts/app.js
@@ -9,34 +9,20 @@ $('.bar').each(function (){
     $(this).addClass(item)
 })
 $(document).ready(function(){
+    let open = false;
 
-    $('#toggleMenu .list').click(function(){
-        $('#sidebar-menu li span').animate({'opacity':1, 'margin-left':'0px'});
-        // $('#sidebar-menu li').removeClass('text-center');
+    $('#hamburger-icon').click(function(){
+        $(this).toggleClass('active');
         $('#sidebar-menu').toggleClass('animate');
-        $('#toggleMenu .list').fadeOut();
-        $('#toggleMenu .thumbs').fadeIn();
-
+        // $('#sidebar-menu li span').toggleClass('toggle');
+        if(!open){
+            open = true
+            $('#sidebar-menu li span').animate({'opacity':1, 'margin-left':'0px'});
+        }
+        else {
+            open = false
+            $('#sidebar-menu li span').css({'opacity': 0, 'margin-left': "10px"});
+        }
     });
-
-    $('#toggleMenu .thumbs').click(function(){
-        $('#sidebar-menu li span').css({'opacity': 0, 'margin-left': "10px"});
-        // $('#sidebar-menu li').addClass('text-center');
-        $('#sidebar-menu').toggleClass('animate');
-        $('#toggleMenu .thumbs').fadeOut();
-        $('#toggleMenu .list').fadeIn();
-
-    });
-
-    $("#sidebar-menu li").click(function(){
-        $("#sidebar-menu li").not(this).removeClass("selected");
-        $(this).toggleClass("selected");
-    });
-
-    $('#drop-select').click(function(){
-        $('#dropdown-list').toggleClass('animate');
-    });
-
-    $('#dropdown-list li').click(function(){$('#dropdown-list').removeClass('animate');});
 
 });

80f9a672f7a6e0a12bde47fbf80d06bde27f7b73
Alex Rans <alex.rans@iodigital.com>
2023-05-22 12:59:35 +0200

    yea

--- htdocs/src/Controller/HomeController.php
+++ htdocs/src/Controller/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Service\Projects;
 use App\Service\Skills;
-use App\Service\SkillsList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
